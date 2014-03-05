f270a84e711244e89849cd6d5c26845bfa55f9f5
Song <nosongyang@gmail.com>
2014-03-05 17:11:48 +0800

    change db config

--- config.py
+++ config.py
@@ -1,19 +1,14 @@
 SRF_ENABLED = True
 SECRET_KEY = 'iwnids-sdf-sdfeg-erewr'
 
-OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DBUSERNAME='root'
-DBPASSWD='123456'
+DBPASSWD='mainliner880320'
 DBHOST='localhost'
-DBNAME='sqlalchemy_test'
+DBNAME='ios_push'
 
 
 SQLALCHEMY_DATABASE_URI = 'mysql://'+DBUSERNAME+':'+DBPASSWD+'@'+DBHOST+'/'+DBNAME
