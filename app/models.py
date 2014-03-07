f15085faa7b82c29d646a703b307a8e9321f94f5
Song <nosongyang@gmail.com>
2014-03-07 11:45:57 +0800

    change db

--- app/models.py
+++ app/models.py
@@ -22,4 +22,26 @@ class User(db.Model):
 
 
 
+class adminUser(db.Model):
+    __tablename__='adminuser'
+    id=db.Column(db.Integer,primary_key=True)
+    nickname = db.Column(db.String(64), unique = True)
+    email = db.Column(db.String(120), unique = True)
 
+    def __init__(self, nickname=None,email=None):
+        self.email=email
+        self.nickname=nickname
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+    def __repr__(self):
+        return '<User %s>' % self.email
